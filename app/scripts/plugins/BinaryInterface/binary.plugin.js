41d4ad619bde421725002caafc10d854c8fe3b1d
Agustin Moyano <agustin.moyano@gmail.com>
2016-09-16 16:12:32 -0300

    se mejora plugin para BinaryInterface

--- app/scripts/plugins/BinaryInterface/binary.plugin.js
+++ app/scripts/plugins/BinaryInterface/binary.plugin.js
@@ -1,7 +1,22 @@
 (function() {
   'use strict';
   var template =
-    '<div>Binary Interface Template</div>';
+    '<div class="md-whiteframe-1dp" layout="column" layout-padding>'+
+      '<form layout="column">'+
+        '<md-button ng-click="upload()">Archivo</md-button>'+
+        '<md-button ng-if="!selectedStore && !internal">'+
+          '<span>Agregar Store</span>'+
+          '<md-icon md-font-set="material-icons">add</md-icon>'+
+        '</md-button>'+
+        '<div ng-if="selectedStore && !internal" layout="row">'+
+          '<span>{{selectedStore.objName}}</span>'+
+          '<md-button>'+
+            '<span>Cambiar Store</span>'+
+            '<md-icon md-font-set="material-icons">sync</md-icon>'+
+          '</md-button>'+
+        '</div>'+
+      '</form>'+
+    '</div>';
   angular.module('binary.plugin', [])
   .directive('binaryInterface', function() {
     return {
@@ -12,8 +27,26 @@
         edit: '<?'
       },
       template: template,
-      controller: function($scope) {
-        console.log('binary');
+      controller: function($scope, io) {
+        $scope.$watch('ngModel', function(value) {
+          if(!value.store) $scope.selectedStore = null;
+          if(!$scope.selectedStore || $scope.selectedStore._id != value.store) {
+            io.emit('get:document', value.store, function(err, doc) {
+              $scope.selectedStore = doc;
+            });
+          }
+          $scope.internal = value.internal;
+        });
+        io.emit('get:schema:named', 'StoreInterface', function(err, int) {
+          io.emit('list:document', {ojbInterface: int._id}, function(cb, list) {
+            $scope.stores = list;
+          });
+        });
+        $scope.upload = function() {
+          angular.element('<input type="file"/>').on('change', function(){
+            console.log(this.files);
+          })[0  ].click();
+        };
       }
     };
   });
